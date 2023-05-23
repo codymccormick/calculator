92ddc8fa068fa4dbbb4fc155ee51a191d8c214df
Cody McCormick <mr.codymccormick@gmail.com>
2023-05-23 17:47:41 -0500

    addded decimal and delete functionality and more dracula themeing

--- script.js
+++ script.js
@@ -10,11 +10,12 @@ const operatorButtons = document.getElementsByClassName("operator");
 const calculateButton = document.getElementById("calculate");
 const clearButton = document.getElementById("clear");
 const decimalButton = document.getElementById("decimal");
+const deleteButton = document.getElementById("delete");
 
-const addition = (num1, num2) => num1 + num2;
-const subtraction = (num1, num2) => num1 - num2;
-const multiplication = (num1, num2) => num1 * num2;
-const division = (num1, num2) => num1 / num2;
+const addition = (num1, num2) => parseFloat(num1) + parseFloat(num2);
+const subtraction = (num1, num2) => parseFloat(num1) - parseFloat(num2);
+const multiplication = (num1, num2) => parseFloat(num1) * parseFloat(num2);
+const division = (num1, num2) => parseFloat(num1) / parseFloat(num2);
 
 const calculate = (num1, num2, operator) => {
   switch (operator) {
@@ -25,25 +26,34 @@ const calculate = (num1, num2, operator) => {
     case "x":
       return multiplication(num1, num2);
     case "/":
-      return division(num1, num2);
+      if (num1 == 0 || num2 == 0) {
+        return "Error";
+      } else {
+        return division(num1, num2);
+      }
   }
+};
+
+const reset = () => {
+  num1 = undefined;
+  num2 = undefined;
+  operator = undefined;
+  currentNumber = "";
+  display.textContent = "0";
 };
 
 for (let button of numberButtons) {
   button.addEventListener("click", function () {
-    if (num1 == result && !operator) {
-      display.textContent = " ";
-      num1 = undefined;
-      result = undefined;
-      currentNumber = "";
+    if (num1 === result && !operator) {
+      reset();
     }
     const number = this.innerText;
     currentNumber += number;
     if (operator) {
-      num2 = parseInt(currentNumber);
+      num2 = currentNumber;
       display.textContent = `${num1} ${operator} ${num2}`;
     } else {
-      num1 = parseInt(currentNumber);
+      num1 = currentNumber;
       display.textContent = num1;
     }
   });
@@ -51,30 +61,58 @@ for (let button of numberButtons) {
 
 for (let button of operatorButtons) {
   button.addEventListener("click", function () {
+    if (num1 && num2) {
+      result = calculate(num1, num2, operator);
+      display.textContent = result;
+      num1 = result;
+      num2 = undefined;
+      operator = undefined;
+    }
     operator = this.innerText;
     display.textContent += " " + this.textContent;
     currentNumber = "";
   });
 }
 
-if (calculateButton) {
-  calculateButton.addEventListener("click", function () {
-    result = calculate(num1, num2, operator);
-    display.textContent = result;
-    num1 = result;
-    num2 = undefined;
+calculateButton.addEventListener("click", function () {
+  if (!num2) {
+    reset();
+  }
+  result = calculate(num1, num2, operator);
+  display.textContent = result;
+  num1 = result;
+  num2 = undefined;
+  operator = undefined;
+});
+
+clearButton.addEventListener("click", reset);
+
+decimalButton.addEventListener("click", function () {
+  if (!currentNumber.includes(".")) {
+    currentNumber += ".";
+    if (operator) {
+      display.textContent = `${num1} ${operator} ${currentNumber}`;
+    } else {
+      display.textContent = currentNumber;
+    }
+  }
+});
+
+deleteButton.addEventListener("click", function () {
+  if (currentNumber === "" && operator) {
     operator = undefined;
-  });
-}
+    display.textContent = num1;
+  } else if (currentNumber !== "") {
+    currentNumber = currentNumber.slice(0, -1);
 
-if (clearButton) {
-  clearButton.addEventListener("click", function () {
-    display.textContent = " ";
-    num1 = undefined;
-    num2 = undefined;
-    operator = undefined;
-    currentNumber = "";
-  });
-}
-
-
+    if (operator && num2) {
+      num2 = currentNumber;
+      display.textContent = `${num1} ${operator} ${num2}`;
+    } else if (operator && !num2) {
+      display.textContent = `${num1} ${operator}`;
+    } else {
+      num1 = currentNumber;
+      display.textContent = num1;
+    }
+  }
+});
